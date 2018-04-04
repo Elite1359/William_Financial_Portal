a26163771a6bff0f84ecdde7af3c6fe053c0c811
Elite1359 <31090549+Elite1359@users.noreply.github.com>
2018-04-04 14:41:08 -0400

    Start

---
+++ William_Financial_Portal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(William_Financial_Portal.Startup))]
+namespace William_Financial_Portal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
